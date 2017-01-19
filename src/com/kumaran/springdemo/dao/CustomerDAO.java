40b1f87be592df23fb60560ee499b729051705e2
mdlkumaran <mdlkumaran@gmail.com>
2017-01-19 11:56:58 -0500

    Final Change

---
+++ src/com/kumaran/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,16 @@
+package com.kumaran.springdemo.dao;
+
+import java.util.List;
+
+import com.kumaran.springdemo.entity.Customer;
+
+
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+}
